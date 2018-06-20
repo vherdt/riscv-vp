079487effdef3ce456c1ec437c66c3f07ca6f303
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2018-06-20 09:26:13 +0200

    example update

--- sw/basic-gcov/main.c
+++ sw/basic-gcov/main.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
+#include "stdio.h"
 
-int main (void) {
-  int i;
-
-  for(i=1;i<13;i++)
-  {
-    if(i%2 == 0)
-    printf("%d is divisible by 2 \n",i);
-
-    if(i%5==0)
-    printf("%d is divisible by 5 \n",i);
-
-    if (i%14==0)
-    printf("%d is divisible by 14 \n",i);
+int main() {
+  int i = 0;
+  
+  while (i < 10) {    
+    if (i < 5) {
+        printf("i < 5\n");
+    }
+    
+    if (i % 2 == 0) {
+        printf("i mod 2 == 0\n");
+    }
+    
+    if (i == 10) {
+        printf("i == 10\n");
+    }
+    
+    ++i;
   }
 
   return 0;
