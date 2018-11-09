9cf2e252fccdacca8c4344d024d08e4b3dd6a448
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2018-11-09 16:59:52 +0100

    make PLIC more robust, allowing it to work even if the SW clears the harts pending interrupt bit

--- vp/lib/plic.cpp
+++ vp/lib/plic.cpp
@@ -100,11 +100,13 @@ void PLIC::register_access_callback(const vp::map::register_access_t &r) {
 
 	if (r.write && r.vptr == &hart_0_claim_response) {
 		//NOTE: on completed response, check if there are any other pending interrupts
-		if (!hart_0_has_pending_enabled_interrupts()) {
-			hart_0_eip = false;
-			target_hart->clear_external_interrupt();
-			//e_run.notify(clock_cycle);
-		}
+		if (hart_0_has_pending_enabled_interrupts()) {
+			assert (hart_0_eip);
+            target_hart->trigger_external_interrupt();  // trigger again to make this work even if the SW clears the harts interrupt pending bit
+		} else {
+            hart_0_eip = false;
+            target_hart->clear_external_interrupt();
+        }
 		//std::cout << "[vp::plic] clear eip" << std::endl;
 	}
 }
