6c89063ea619d114c224d79303849687eb16d61b
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2019-03-06 01:30:45 +0100

    fix indentation

--- vp/src/util/tlm_map.h
+++ vp/src/util/tlm_map.h
@@ -205,7 +205,7 @@ struct RegisterMapping : public AbstractMapping {
         // introduce *nv* to get rid of "use of uninitialized value" warnings
         uint32_t nv = 0;
         if (cmd == tlm::TLM_WRITE_COMMAND)
-        	nv = *new_vptr;
+            nv = *new_vptr;
 
 		handler({cmd == tlm::TLM_READ_COMMAND, cmd == tlm::TLM_WRITE_COMMAND, r.vptr, nv, fn, delay, addr});
 		return true;
