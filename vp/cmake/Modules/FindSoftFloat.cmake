68d81477a2998b0bd81ed03c6bcb5c549ab16cde
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2019-03-07 19:56:03 +0100

    add build scripts for SoftFloat library

---
+++ vp/cmake/Modules/FindSoftFloat.cmake
@@ -0,0 +1,5 @@
+set(SoftFloat_DIR "${CMAKE_SOURCE_DIR}/dependencies/softfloat-dist")
+
+set(SoftFloat_INCLUDE_DIRS ${SoftFloat_DIR}/include )
+set(SoftFloat_INCLUDE_DIR  ${SoftFloat_INCLUDE_DIRS} )
+set(SoftFloat_LIBRARIES    ${SoftFloat_DIR}/lib/libsoftfloat.a )
