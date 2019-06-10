d3ae992a4b8b3a563901d31669ec2ce2a94fa94e
Sören Tempel <soeren+git@soeren-tempel.net>
2019-06-10 13:45:53 +0200

    core: move PrivilegeLevel declaration to common/
    Values and types are the same for rv32 and rv64. Additionally, moving
    this declaration to common/ allows using it from other headers in
    common/, e.g. plic.h.

--- vp/src/core/common/irq_if.h
+++ vp/src/core/common/irq_if.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
+typedef uint32_t PrivilegeLevel;
+
+constexpr uint32_t MachineMode = 0b11;
+constexpr uint32_t SupervisorMode = 0b01;
+constexpr uint32_t UserMode = 0b00;
+constexpr uint32_t NoneMode = -1;    // invalid sentinel to avoid passing a boolean alongside a privilege level
+
 struct external_interrupt_target {
 	virtual ~external_interrupt_target() {}
 
