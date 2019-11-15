1314a80edf6a006ef600011fca87a31870fe2b0a
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2019-11-15 21:55:54 +0100

    decoder ecall and ebreak mask fix && trap on reserved instructions

--- vp/src/core/common/instr.cpp
+++ vp/src/core/common/instr.cpp
@@ -92,9 +92,9 @@ constexpr uint32_t FENCE_MASK = 0b00000000000000000111000001111111;
 constexpr uint32_t FENCE_ENCODING = 0b00000000000000000000000000001111;
 constexpr uint32_t FENCE_I_MASK = 0b00000000000000000111000001111111;
 constexpr uint32_t FENCE_I_ENCODING = 0b00000000000000000001000000001111;
-constexpr uint32_t ECALL_MASK = 0b11111111111100000111000001111111;
+constexpr uint32_t ECALL_MASK = 0b11111111111111111111111111111111;
 constexpr uint32_t ECALL_ENCODING = 0b00000000000000000000000001110011;
-constexpr uint32_t EBREAK_MASK = 0b11111111111100000111000001111111;
+constexpr uint32_t EBREAK_MASK = 0b11111111111111111111111111111111;
 constexpr uint32_t EBREAK_ENCODING = 0b00000000000100000000000001110011;
 constexpr uint32_t CSRRW_MASK = 0b00000000000000000111000001111111;
 constexpr uint32_t CSRRW_ENCODING = 0b00000000000000000001000001110011;
@@ -1135,7 +1135,7 @@ Opcode::Mapping expand_compressed(Instruction &instr, Compressed::Opcode op, Arc
 			return UNDEF;
 
 		case C_Reserved:
-			throw std::runtime_error("Reserved compressed instruction detected");
+		    return UNDEF;   // reserved instructions should raise an illegal instruction exception
 
 		case C_NOP:
 			instr = InstructionFactory::ADD(0, 0, 0);
@@ -1222,6 +1222,8 @@ Opcode::Mapping expand_compressed(Instruction &instr, Compressed::Opcode op, Arc
 			return JAL;
 
 		case C_ADDIW:
+            if (instr.c_rd() == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::ADDI(instr.c_rd(), instr.c_rd(), instr.c_imm());
 			return ADDIW;
 
@@ -1231,16 +1233,16 @@ Opcode::Mapping expand_compressed(Instruction &instr, Compressed::Opcode op, Arc
 
 		case C_ADDI16SP: {
 			auto n = C_ADDI16SP_NZIMM(instr.data());
-			assert(n != 0);
+            if (n == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::ADDI(2, 2, n);
 			return ADDI;
 		}
 
 		case C_LUI: {
 			auto n = C_LUI_NZIMM(instr.data());
-			assert(n != 0);
-			assert(instr.c_rd() != 0);
-			assert(instr.c_rd() != 2);
+            if (n == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::LUI(instr.c_rd(), n);
 			return LUI;
 		}
@@ -1286,10 +1288,14 @@ Opcode::Mapping expand_compressed(Instruction &instr, Compressed::Opcode op, Arc
 			return BNE;
 
 		case C_LWSP:
+            if (instr.c_rd() == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::LW(instr.c_rd(), 2, C_LWSP_UIMM(instr.data()));
 			return LW;
 
 		case C_LDSP:
+            if (instr.c_rd() == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::LD(instr.c_rd(), 2, C_LDSP_UIMM(instr.data()));
 			return LD;
 
@@ -1322,6 +1328,8 @@ Opcode::Mapping expand_compressed(Instruction &instr, Compressed::Opcode op, Arc
 			return EBREAK;
 
 		case C_JR:
+            if (instr.c_rd() == 0)
+                return UNDEF;	// reserved
 			instr = InstructionFactory::JALR(0, instr.c_rd(), 0);
 			return JALR;
 
