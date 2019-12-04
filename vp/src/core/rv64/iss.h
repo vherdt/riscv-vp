df5ab531769dd3bf6d9a943d8b3f514bde8ca8f7
Sören Tempel <tempel@uni-bremen.de>
2019-12-04 16:04:16 +0100

    debug: add debug_mode to debugable

--- vp/src/core/rv64/iss.h
+++ vp/src/core/rv64/iss.h
@@ -156,8 +156,6 @@ struct ISS : public external_interrupt_target, public clint_interrupt_target, pu
 	Instruction instr;
 	Opcode::Mapping op;
 
-	bool debug_mode = false;
-
 	sc_core::sc_event wfi_event;
 
 	std::string systemc_name;
